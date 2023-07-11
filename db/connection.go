d804feca165c3a9a99c72d1ae3e69c684b346d7b
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2023-07-11 17:25:39 -0300

    Added connection file, entities and insert files.

--- db/connection.go
+++ db/connection.go
@@ -1,7 +1,26 @@
-// package db
+package db
 
-// import "github.com/CordeiroAboumrad/first_go_api/configs"
+import (
+	"database/sql"
+	"fmt"
 
-// func openConnection(){
+	"github.com/CordeiroAboumrad/first_go_api/configs"
+	_ "github.com/lib/pq"
+)
 
-// }
+func openConnection() (*sql.DB, error) {
+	config := configs.GetDB()
+
+	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		config.Host, config.Port, config.User, config.Password, config.Database)
+
+	conn, err := sql.Open("postgres", sc)
+	if err != nil {
+		// Mudar para produção
+		panic(err)
+	}
+
+	err = conn.Ping()
+
+	return conn, err
+}
---
+++ models/insert.go
@@ -0,0 +1,11 @@
+// package models
+
+// import (
+// 	"github.com/CordeiroAboumrad/first_go_api/db"
+// )
+
+// func Insert(todo Todo) (id int64, err error) {
+// 	conn, err := db.
+
+// 	return
+// }
