2f1d505e34b7bf07933050b5367c0ced2ea9d93d
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2023-07-11 17:45:35 -0300

    added method to get all from db

--- models/insert.go
+++ models/insert.go
@@ -1,11 +1,21 @@
-// package models
+package models
 
-// import (
-// 	"github.com/CordeiroAboumrad/first_go_api/db"
-// )
+import (
+	"github.com/CordeiroAboumrad/first_go_api/db"
+)
 
-// func Insert(todo Todo) (id int64, err error) {
-// 	conn, err := db.
+func Insert(todo Todo) (id int64, err error) {
+	conn, err := db.OpenConnection()
 
-// 	return
-// }
+	if err != nil {
+		return
+	}
+
+	defer conn.Close()
+
+	sql := `INSERT INTO todos (title, description, done) VALUES ($1, $2, $3) RETURN id`
+
+	err = conn.QueryRow(sql, todo.Title, todo.Description, todo.Done).Scan(&id)
+
+	return
+}
